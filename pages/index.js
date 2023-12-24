b3b855cdb2835c5ffc87364bc643efd843765476
Felipe <felipe.puziol@gmail.com>
2023-12-24 16:49:54 +0000

    feat: add welcome christmas page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <div className="Home">
+    <h1>Feliz Natal!</h1>
+    <p>Que esta época seja repleta de alegria e amor para você e sua família.</p>
+  </div>
+}
+
+export default Home;
