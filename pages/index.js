fbb9c7fb714c4b4f47f1de4cab04554ece9eec3b
Felipe <felipe.puziol@gmail.com>
2024-01-09 03:46:37 +0000

    style: add script `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 function Home() {
-    return <div className="Home">
-    <h1>Feliz Natal!</h1>
-    <p>Que esta época seja repleta de alegria e amor para você e sua família.</p>
-  </div>
+  return (
+    <div className="Home">
+      <h1>Feliz Natal!</h1>
+      <p>
+        Que esta época seja repleta de alegria e amor para você e sua família.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
