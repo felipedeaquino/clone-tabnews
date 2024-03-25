282c8e9950ffa0b0d69b939fa636d4cd60a9c201
felipedeaquino <felipe.puziol@gmail.com>
2024-03-25 07:03:51 -0300

    add `api/v1/status` endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,5 @@
+function status(req, res) {
+  res.status(200).json({ status: "ok" });
+}
+
+export default status;
---
+++ tests/unit/test.test.js
@@ -0,0 +1,3 @@
+test('adds 1 + 3 to equal 3', () => {
+  expect( 1 + 2 ).toBe(3);
+})
